fa8d2ff9f5580cea1ef5d81024a77917f6a800a2
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-11-18 13:51:24 -0500

    Added Spring starter jpa dependency, added jpa annotations to Ingredient, Taco, and Order

--- taco-cloud/src/main/java/tacos/Order.java
+++ taco-cloud/src/main/java/tacos/Order.java
@@ -1,8 +1,17 @@
 package tacos;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.CreditCardNumber;
@@ -10,7 +19,16 @@ import javax.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class Order {
+@Entity
+@Table(name="Taco_Order")
+public class Order implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
 	private Date placedAt;
@@ -39,10 +57,16 @@ public class Order {
 	@Digits(integer=3, fraction=0, message="invalid cvv")
 	private String ccCVV;
 	
+	@ManyToMany(targetEntity=Taco.class)
 	private List<Taco> tacos = new ArrayList<>();
 	
 	public void addDesign(Taco design) {
 		this.tacos.add(design);
+	}
+	
+	@PrePersist
+	void placedAt() {
+		this.placedAt = new Date();
 	}
 	
 }
--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -1,12 +1,24 @@
 package tacos;
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
+	
+	@Id
 	private long id;
 	
 	private Date createdAt;
@@ -14,6 +26,13 @@ public class Taco {
 	@NotNull
 	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
+	
+	@ManyToMany(targetEntity=Ingredient.class)
 	@Size(min=1, message="You must choose at least 1 ingredient")
 	private List<Ingredient> ingredients;
+	
+	@PrePersist
+	void createdAt() {
+		this.createdAt = new Date();
+	}
 }
