a1146a67d493eae2b02ea3ce0de68209e274653d
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-11-19 14:28:27 -0500

    commented out jdbc repository implementation classes. Added methods to ordererepository

--- taco-cloud/src/main/java/tacos/data/JdbcTacoRepository.java
+++ taco-cloud/src/main/java/tacos/data/JdbcTacoRepository.java
@@ -1,59 +1,62 @@
 package tacos.data;
 
-import java.sql.Timestamp;
-import java.sql.Types;
-import java.util.Arrays;
-import java.util.Date;
+//import java.sql.Timestamp;
+//import java.sql.Types;
+//import java.util.Arrays;
+//import java.util.Date;
+//
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.jdbc.core.PreparedStatementCreator;
+//import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+//import org.springframework.jdbc.support.GeneratedKeyHolder;
+//import org.springframework.jdbc.support.KeyHolder;
+//import org.springframework.stereotype.Repository;
+//
+//import tacos.Ingredient;
+//import tacos.Taco;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
+//Empty class to take place of commented out code
+public class JdbcTacoRepository {}
 
-import tacos.Ingredient;
-import tacos.Taco;
-
-@Repository
-public class JdbcTacoRepository implements TacoRepository {
-	
-	private JdbcTemplate jdbc;
-	
-	public JdbcTacoRepository(JdbcTemplate jdbc) {
-		this.jdbc = jdbc;
-	}
-
-	@Override
-	public Taco save(Taco taco) {
-		long tacoId = saveTacoInfo(taco);
-		taco.setId(tacoId);
-		for (Ingredient ingredient : taco.getIngredients()) {
-			saveIngredientToTaco(ingredient, tacoId);
-		}
-		return taco;
-	}
-	
-	private long saveTacoInfo(Taco taco) {
-		taco.setCreatedAt(new Date());
-		PreparedStatementCreatorFactory preparedStatementCreatorFactory = 
-				new PreparedStatementCreatorFactory(
-				"insert into Taco (name, createdAt) values(?, ?)", 
-				Types.VARCHAR, Types.TIMESTAMP);
-		preparedStatementCreatorFactory.setReturnGeneratedKeys(true);
-//		System.out.println("==============Look HERE=============\nTaco name:"  + taco.getName() + "\nTimestamp" + taco.getCreatedAt().getTime());
-		PreparedStatementCreator psc = preparedStatementCreatorFactory
-				.newPreparedStatementCreator(
-						Arrays.asList(taco.getName(), 
-						new Timestamp(taco.getCreatedAt().getTime())));
-		KeyHolder keyholder = new GeneratedKeyHolder();
-		int rowsAffected = (int)jdbc.update(psc, keyholder);
-		System.out.println("Rows affected: " + rowsAffected);
-		System.out.println("Key: " + keyholder.getKey());
-		return keyholder.getKey().longValue();
-	}
-	
-	private void saveIngredientToTaco(Ingredient ingredient, long tacoId) {
-		jdbc.update("insert into Taco_Ingredients (taco, ingredient) values (?, ?)", tacoId, ingredient.getId());
-	}
-}
+//@Repository
+//public class JdbcTacoRepository implements TacoRepository {
+//	
+//	private JdbcTemplate jdbc;
+//	
+//	public JdbcTacoRepository(JdbcTemplate jdbc) {
+//		this.jdbc = jdbc;
+//	}
+//
+//	@Override
+//	public Taco save(Taco taco) {
+//		long tacoId = saveTacoInfo(taco);
+//		taco.setId(tacoId);
+//		for (Ingredient ingredient : taco.getIngredients()) {
+//			saveIngredientToTaco(ingredient, tacoId);
+//		}
+//		return taco;
+//	}
+//	
+//	private long saveTacoInfo(Taco taco) {
+//		taco.setCreatedAt(new Date());
+//		PreparedStatementCreatorFactory preparedStatementCreatorFactory = 
+//				new PreparedStatementCreatorFactory(
+//				"insert into Taco (name, createdAt) values(?, ?)", 
+//				Types.VARCHAR, Types.TIMESTAMP);
+//		preparedStatementCreatorFactory.setReturnGeneratedKeys(true);
+////		System.out.println("==============Look HERE=============\nTaco name:"  + taco.getName() + "\nTimestamp" + taco.getCreatedAt().getTime());
+//		PreparedStatementCreator psc = preparedStatementCreatorFactory
+//				.newPreparedStatementCreator(
+//						Arrays.asList(taco.getName(), 
+//						new Timestamp(taco.getCreatedAt().getTime())));
+//		KeyHolder keyholder = new GeneratedKeyHolder();
+//		int rowsAffected = (int)jdbc.update(psc, keyholder);
+//		System.out.println("Rows affected: " + rowsAffected);
+//		System.out.println("Key: " + keyholder.getKey());
+//		return keyholder.getKey().longValue();
+//	}
+//	
+//	private void saveIngredientToTaco(Ingredient ingredient, long tacoId) {
+//		jdbc.update("insert into Taco_Ingredients (taco, ingredient) values (?, ?)", tacoId, ingredient.getId());
+//	}
+//}
--- taco-cloud/src/main/java/tacos/data/OrderRepository.java
+++ taco-cloud/src/main/java/tacos/data/OrderRepository.java
@@ -1,8 +1,20 @@
 package tacos.data;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import tacos.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
+	// methods formed as verbSubjectByPredicate where Subject is optional
+	// so here find is the verb, and DeliveryZip is the property to match the passed parameter with
+	List<Order> findByDeliveryZip(String deliveryZip);
+	/*	in this more complicated method name, read is the verb (synonymous with get and find) could also 
+	 * 	use count, Orders is the optional subject (spring actually ignores most subjects and uses
+	 * 	the parameter), the predicate refers to two order properties separated by And.  DeliveryZip must 
+	 *  match the first parameter, and placedAt is specified to match if between second two */ 
+	List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+	
 }
