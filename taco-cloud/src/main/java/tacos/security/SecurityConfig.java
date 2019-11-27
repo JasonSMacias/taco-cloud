91d29e11421a04549b632e42e8073ed74ecb9f08
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-11-27 18:03:06 -0500

    Added user repository and user details service in security, updated securityconfig to use this Spring Data repository for User to store data (instead of jdbc)

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -3,11 +3,13 @@ package tacos.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
@@ -18,18 +20,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	DataSource dataSource;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+		
 		//Below code for setting up in-memory user store for two users defined directly in configuration
 //		auth.inMemoryAuthentication().withUser("buzz").password("infinity").authorities("ROLE_USER")
 //			.and().withUser("woody").password("bullseye").authorities("Role_User");
 		
 		// Below is the code for authenticating against a jdbc-based user store with custom queries
 		// If I just want to use the default queries, the last two chained methods are not necessary
-		auth.jdbcAuthentication().dataSource(dataSource)
-			.usersByUsernameQuery("select username, password, enabled from Users where username=?")
-			.authoritiesByUsernameQuery("select username, authority from UserAuthorities where username=?")
-			.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+//		auth.jdbcAuthentication().dataSource(dataSource)
+//			.usersByUsernameQuery("select username, password, enabled from Users where username=?")
+//			.authoritiesByUsernameQuery("select username, authority from UserAuthorities where username=?")
+//			.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
 		
 		// Below is the code for authenication using LDAP (lightweight directory access protocol)
 		// optional userSearchBase and groupSearchBase methods indicate where in the ldap hierarchy 
@@ -40,5 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		.passwordCompare().passwordEncoder(new BCryptPasswordEncoder())
 //		.passwordAttribute("passcode")
 //		.contextSource().url("ldap://tacocloud.com:389/dc=tacocloud,dc=com");
+		
+		
 	}
 }
