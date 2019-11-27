af60ae29197125520662860fd1343f9376dafb06
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-11-27 16:52:30 -0500

    Added password encoding in security configure and example of using LDAP to access a user store

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -3,12 +3,15 @@ package tacos.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,7 +28,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// If I just want to use the default queries, the last two chained methods are not necessary
 		auth.jdbcAuthentication().dataSource(dataSource)
 			.usersByUsernameQuery("select username, password, enabled from Users where username=?")
-			.authoritiesByUsernameQuery("select username, authority from UserAuthorities where username=?");
-			
+			.authoritiesByUsernameQuery("select username, authority from UserAuthorities where username=?")
+			.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+		
+		// Below is the code for authenication using LDAP (lightweight directory access protocol)
+		// optional userSearchBase and groupSearchBase methods indicate where in the ldap hierarchy 
+		// the search will begin, default is root
+//		auth.ldapAuthentication()
+//		.userSearchBase("ou=people").userSearchFilter("(uid={0})")
+//		.groupSearchBase("ou=groups").groupSearchFilter("member={0}")
+//		.passwordCompare().passwordEncoder(new BCryptPasswordEncoder())
+//		.passwordAttribute("passcode")
+//		.contextSource().url("ldap://tacocloud.com:389/dc=tacocloud,dc=com");
 	}
 }
