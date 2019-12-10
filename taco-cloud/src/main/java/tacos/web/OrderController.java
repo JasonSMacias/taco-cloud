473ec2bf21659fb4a59257c57cdd6c7a495e03f0
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-12-10 11:16:40 -0500

    In order bean added many to one relationship with user, and in order controller, added secutiry annotated user to processOrder method params and setuser method within.

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,6 +1,7 @@
 package tacos.web;
 import javax.validation.Valid;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.support.SessionStatus;
 //import lombok.extern.slf4j.Slf4j;
 
 import tacos.Order;
+import tacos.User;
 import tacos.data.OrderRepository;
 
 //@Slf4j
@@ -33,12 +35,19 @@ public class OrderController {
 	}
 	
 	@PostMapping
-	public String prosessOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+	public String prosessOrder(
+			@Valid Order order, Errors errors, SessionStatus sessionStatus, 
+			@AuthenticationPrincipal User user) {
+		
 		if(errors.hasErrors()) {
 			return "orderForm";
 		}
+		
+		order.setUser(user);
+		
 		orderRepo.save(order);
 		sessionStatus.setComplete();
+		
 		return "redirect:/";
 	}
 }
