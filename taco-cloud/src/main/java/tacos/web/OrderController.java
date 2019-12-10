e58d108f4dfc88512fb4dc942b8b3a6c9b40a9c0
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-12-10 16:02:51 -0500

    Added missing authentication into order controller /current get with conditionals from online example

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -30,7 +31,24 @@ public class OrderController {
 	}
 	
 	@GetMapping("/current")
-	public String orderForm() {
+	public String orderForm(@AuthenticationPrincipal User user, @ModelAttribute Order order) {
+		
+		if (order.getDeliveryName() == null) {
+	      order.setDeliveryName(user.getFullname());
+	    }
+	    if (order.getDeliveryStreet() == null) {
+	      order.setDeliveryStreet(user.getStreet());
+	    }
+	    if (order.getDeliveryCity() == null) {
+	      order.setDeliveryCity(user.getCity());
+	    }
+	    if (order.getDeliveryState() == null) {
+	      order.setDeliveryState(user.getState());
+	    }
+	    if (order.getDeliveryZip() == null) {
+	      order.setDeliveryZip(user.getZip());
+	    }
+		
 		return "orderForm";
 	}
 	
